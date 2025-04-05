04f98732f992662faa43bd88d0e11266647fa93f
Asteroidvr101 <projectinfectedoffical@gmail.com>
2025-04-04 20:13:39 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,9 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+TitleId = "7AF94"
+
+@app.route("/", methods=["POST", "GET"])
+def title_data():
+    response = requests.post(
+        url=f"https://{settings.TitleId}.playfabapi.com/Server/GetTitleData",
+        headers=settings.get_auth_headers()
+    )
+
+    if response.status_code == 200:
+        return jsonify(response.json().get("data").get("Data"))
+    else:
+        return jsonify({}), response.status_code
 
 @app.route('/about')
 def about():
