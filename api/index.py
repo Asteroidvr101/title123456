70f9b0b41540782f1945c920c0f5e7d495ccdbd0
Asteroidvr101 <projectinfectedoffical@gmail.com>
2025-04-04 20:15:59 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,11 +6,14 @@ TitleId = "7AF94"
 
 @app.route("/", methods=["POST", "GET"])
 def title_data():
-    response = requests.post(
+     response = requests.post(
         url=f"https://{settings.TitleId}.playfabapi.com/Server/GetTitleData",
-        headers=settings.get_auth_headers()
-    )
-    return jsonify(response.json().get("data").get("Data"))
+        headers=settings.get_auth_headers())
+
+    if response.status_code == 200:
+        return jsonify(response.json().get("data").get("Data"))
+    else:
+        return jsonify({}), response.status_code
 
 @app.route('/about')
 def about():
